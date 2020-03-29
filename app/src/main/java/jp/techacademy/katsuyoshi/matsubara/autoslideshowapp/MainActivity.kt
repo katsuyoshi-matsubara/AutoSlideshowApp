ba920b2a9f7eab35198a16e164849dfdbdbfd12e
katsuyoshi-matsubara <unikamonec@gmail.com>
2020-03-29 17:38:19 +0900

    再生/停止ボタンに表示の入れ替え、カウントアップタイマー機能を追加

--- app/src/main/java/jp/techacademy/katsuyoshi/matsubara/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/katsuyoshi/matsubara/autoslideshowapp/MainActivity.kt
@@ -39,24 +39,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         playButton.setOnClickListener {
-            mTimer = Timer()
-            mTimer!!.schedule(object : TimerTask() {
-                override fun run() {
-                    mTimerSec += 0.1
-                    mHandler.post {
-                        timer.text = String.format("%.2f", mTimerSec)
-                    }
+            if (mTimer == null) {
+                mTimer = Timer()
+                if (playButton.text == "再生") {
+                    mTimer!!.schedule(object : TimerTask() {
+                        override fun run() {
+                            mTimerSec = (mTimerSec + 0.1) % 2
+                            //mTimerSecを2で割った余り→1.9の後リセット
+                            //リセットされないタイマーと並べて2.0秒と確認済み
+                            mHandler.post {
+                                timer.text = String.format("%.1f", mTimerSec)
+                            }
+                        }
+                    }, 100, 100)
+                    playButton.text = "停止"
                 }
-            }, 100, 100)
-        }
+            } else {//if(mTimer != null)は、上のif文(mTimer==null) のelseなので抜いた。
+                //"停止"でカウントをリセットしてしまう
+                mTimerSec = 0.0 //これも次の行のもないとおかしくなる
+                timer.text = "0.0"
+                mTimer!!.cancel()
+                mTimer = null
+                playButton.text = "再生"
+            }
 
-        forwardButton.setOnClickListener {
 
-        }
-        backButton.setOnClickListener {
-            
-        }
 
+            forwardButton.setOnClickListener {
+                getContentsInfo()
+
+
+            }
+            backButton.setOnClickListener {
+
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -74,24 +91,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun getContentsInfo() {
         // 画像の情報を取得する
-        val resolver = contentResolver
-        val cursor = resolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+        val resolver = contentResolver //ContentResolverはContentProviderのデータを参照するためのクラス
+        val cursor = resolver.query( //ContentResolverクラスのqueryメソッドを使って条件を指定して検索
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // 第1引数データの種類
             null, // 項目(null = 全項目)
             null, // フィルタ条件(null = フィルタなし)
             null, // フィルタ用パラメータ
             null // ソート (null ソートなし)
-        )
+        ) //結果はCursorクラス、Cursorとはデータベース上の検索結果を格納するもの
 
         if (cursor!!.moveToFirst()) {
+            //moveToFirstメソッドで先頭を指し、moveToNextメソッドで順番に移動
+            //どちらのメソッドも情報があり指すことができたらtrue無ければfalseを返す
+            //indexからIDを取得し、そのIDから画像のURIを取得する
 
-            // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
-            val imageUri =
-                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-            imageView.setImageURI(imageUri)
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                //cursorが指しているデータの中から画像のIDがセットされている位置を取得
+                val id = cursor.getLong(fieldIndex)//cursor.getLong()で画像のIDを取得
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                //ContentUris.withAppendedId()でそこから実際の画像のURIを取得
+                imageView.setImageURI(imageUri)
+            //Log.d("ANDROID", "URI : " + imageUri.toString())
+            // } (do) while(cursor.moveToNext())で表示
         }
         cursor.close()
     }
